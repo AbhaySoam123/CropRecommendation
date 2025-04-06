0e9aad8e6bfde1e88a8407f227cb24b7deca41a0
AbhaySoam123 <127785616+AbhaySoam123@users.noreply.github.com>
2025-04-06 13:07:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
+import os  # Add this import
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def predict():
     return render_template('index.html', prediction=crop)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port
+    app.run(debug=False, host='0.0.0.0', port=port)
